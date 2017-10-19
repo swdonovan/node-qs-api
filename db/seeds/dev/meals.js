f7d4ff37bfbd5ae936177cbc15eb24a3fb2cc717
Wil Donovan <wil.donovan@gmail.com>
2017-10-19 11:13:30 -0600

    moving of the seed file so the meals go in under sequential order

--- db/seeds/dev/meals.js
+++ db/seeds/dev/meals.js
@@ -8,11 +8,11 @@ exports.seed = function(knex, Promise) {
       ),
       knex.raw(
         'INSERT INTO meals (name, created_at, updated_at) VALUES (?, ?, ?)',
-        ["Dinner", new Date, new Date]
+        ["Lunch", new Date, new Date]
       ),
       knex.raw(
         'INSERT INTO meals (name, created_at, updated_at) VALUES (?, ?, ?)',
-        ["Lunch", new Date, new Date]
+        ["Dinner", new Date, new Date]
       ),
       knex.raw(
         'INSERT INTO meals (name, created_at, updated_at) VALUES (?, ?, ?)',
